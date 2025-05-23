4a8819aa6ae9c7beccdd772d245aecbb2923f34f
AswinPuthukaatil <aswin100396@gmail.com>
2025-05-23 12:06:47 +0530

    Added a tenant table to make seperation of tenant metadata

---
+++ internal/model/tenant.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"time"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Tenant struct {
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name        string    `gorm:"uniqueIndex;not null" json:"name"`
+	Description string    `json:"description"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (t *Tenant) BeforeCreate(tx *gorm.DB) (err error) {
+	t.ID = uuid.New()
+	return
+}
