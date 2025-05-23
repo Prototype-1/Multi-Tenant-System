147a91b5f02716d00feddc0e18d328184c3f1216
AswinPuthukaatil <aswin100396@gmail.com>
2025-05-24 00:31:50 +0530

    fixed the panic happened due to not converting UUID in handler using safe conversion inside the function

--- internal/handler/location_handler.go
+++ internal/handler/location_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 	"github.com/google/uuid"
 	"github.com/gin-gonic/gin"
 	"github.com/Prototype-1/Multi-Tenant-System/internal/model"
@@ -25,10 +26,14 @@ type CreateLocationRequest struct {
 
 func (h *LocationHandler) CreateLocation(c *gin.Context) {
 	var req CreateLocationRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+   if err := c.ShouldBindJSON(&req); err != nil {
+        if strings.Contains(err.Error(), "Latitude") || strings.Contains(err.Error(), "Longitude") {
+            c.JSON(http.StatusBadRequest, gin.H{"error": "You need to provide both longitude and latitude to continue with this operation"})
+            return
+        }
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 
 	role := c.MustGet("role").(string)
 	if role == "admin" {
@@ -36,8 +41,20 @@ func (h *LocationHandler) CreateLocation(c *gin.Context) {
 		return
 	}
 
-	userID := c.MustGet("user_id").(uuid.UUID)
-	tenantID := c.MustGet("tenant_id").(uuid.UUID)
+	 userIDStr := c.MustGet("user_id").(string)
+    tenantIDStr := c.MustGet("tenant_id").(string)
+    
+    userID, err := uuid.Parse(userIDStr)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid user ID"})
+        return
+    }
+    
+    tenantID, err := uuid.Parse(tenantIDStr)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid tenant ID"})
+        return
+    }
 
 	location := model.Location{
 		UserID:    userID,
@@ -50,6 +67,8 @@ func (h *LocationHandler) CreateLocation(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save location"})
 		return
 	}
-
-	c.JSON(http.StatusCreated, location)
+	   c.JSON(http.StatusCreated, gin.H{
+        "message": "Location saved successfully",
+        "location": location,
+    })
 }
