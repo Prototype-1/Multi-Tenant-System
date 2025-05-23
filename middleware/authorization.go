89fd8dd67f9d0faed7acd15419f25065e2d8020f
AswinPuthukaatil <aswin100396@gmail.com>
2025-05-23 12:05:43 +0530

    updated middleware to include the jwt claims logic and role RBAC based on the requirement

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -1,1 +1,52 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+	"github.com/Prototype-1/Multi-Tenant-System/pkg"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid Authorization header"})
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := pkg.ParseAccessToken(tokenStr)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token: " + err.Error()})
+			return
+		}
+		c.Set("user_id", claims.UserID)
+		c.Set("tenant_id", claims.TenantID)
+		c.Set("role", claims.Role)
+
+		c.Next()
+	}
+}
+
+func AuthorizeRole(allowedRoles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		roleVal, exists := c.Get("role")
+		if !exists {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Role not found in token"})
+			return
+		}
+
+		role := roleVal.(string)
+
+		for _, allowed := range allowedRoles {
+			if role == allowed {
+				c.Next()
+				return
+			}
+		}
+
+		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Access denied for role: " + role})
+	}
+}
