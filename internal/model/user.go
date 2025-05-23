9f3c529b55c4f9cfce1ff94716fbf61530bd9774
AswinPuthukaatil <aswin100396@gmail.com>
2025-05-23 12:32:45 +0530

    Made one to many relation for tenant_id

--- internal/model/user.go
+++ internal/model/user.go
@@ -10,6 +10,7 @@ import (
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	TenantID  uuid.UUID `gorm:"type:uuid;index" json:"tenant_id"`
+	Tenant    Tenant    `gorm:"foreignKey:TenantID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
 	Password  string    `gorm:"not null" json:"-"`
 	Role      string    `gorm:"type:varchar(10);not null" json:"role"` 
